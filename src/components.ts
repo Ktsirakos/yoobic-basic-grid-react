0ec2de83ee956d58cd7732976f95af02d77b3fac
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2020-09-11 01:27:50 +0300

    Commiting react library

---
+++ src/components.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'yoobic-basic-grid';
+
+
+
+export const YoobicBasicGrid = /*@__PURE__*/createReactComponent<JSX.YoobicBasicGrid, HTMLYoobicBasicGridElement>('yoobic-basic-grid');
